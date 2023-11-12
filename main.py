963a122f7ba884672d79254d8d3641541ec9d362
Muhammed Okuyucu <muhammed.okuyucu@hepsiburada.com>
2023-11-12 19:38:38 +0300

    aökdjwka

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from flask import Flask  
+#from flask import Flask  
+#
+#
+#import random
+#
+#
+#list_of_questions_which_already_asked = []
+#
+#def generate_random_number():
+#    return random.randint(1, 45)
+#  
+#app = Flask(__name__) #creating the Flask class object   
+# 
+#@app.route('/') #decorator drfines the   
+#def home():  
+#    random_number = generate_random_number()
+#    if random_number not in list_of_questions_which_already_asked:
+#        print("Random number between 1 and 45:", random_number)
+#
+#    return  str(random_number)
+#  
+#if __name__ =='__main__':  
+#    app.run(debug = True)  
+
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-import random
+@app.route("/")
+def home():
+    return render_template('home.html')
 
 
-list_of_questions_which_already_asked = []
-
-def generate_random_number():
-    return random.randint(1, 45)
-  
-app = Flask(__name__) #creating the Flask class object   
- 
-@app.route('/') #decorator drfines the   
-def home():  
-    random_number = generate_random_number()
-    if random_number not in list_of_questions_which_already_asked:
-        print("Random number between 1 and 45:", random_number)
-
-    return  str(random_number)
-  
-if __name__ =='__main__':  
-    app.run(debug = True)  
+if __name__ == '__main__':
+    app.run(debug=True)
